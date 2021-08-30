e5dbd14bdb091f2b312696bd0cba763e3a901f84
webB1an <webB1anyaoyao@gmail.com>
2021-08-30 16:31:41 +0800

    feat: add pos spider

--- routes/index.ts
+++ routes/index.ts
@@ -5,6 +5,8 @@ import emoji from './emoji'
 import emoji1 from './emoji1'
 import avatar from './avatar'
 
+import pos from './pos'
+
 const router: express.Router = express.Router()
 
 router.use('/movie', movie)
@@ -12,5 +14,7 @@ router.use('/one', one)
 router.use('/emoji', emoji)
 router.use('/emoji1', emoji1)
 router.use('/avatar', avatar)
+
+router.use('/pos', pos)
 
 export default router
