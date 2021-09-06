fcafb9398175dadd7150fdc24c8513351f7015fc
webB1an <webB1anyaoyao@gmail.com>
2021-09-06 09:16:45 +0800

    style: optimize code

--- routes/movie.ts
+++ routes/movie.ts
@@ -29,7 +29,9 @@ router.get('/spider', async(req: Request, res: Response): Promise<Response> => {
   let movies: Movie[] = []
 
   for (const url of urls) {
-    movies = [...movies, ...await parsePage(page, url)]
+    const movie: Movie[] = await parsePage(page, url)
+    console.log('---------------movie---------------', movie, movie.length)
+    movies = [...movies, ...movie]
   }
 
   for (const index in movies) {
@@ -75,70 +77,80 @@ async function parsePage(page: puppeteer.Page, url: string): Promise<Movie[]> {
   let movie: Movie[] = []
   const selector = '.post-box'
 
-  // 跳转页面
-  await page.goto(url)
+  try {
+    // 跳转页面
+    await page.goto(url)
 
-  // 绑定 console
-  await page.on('console', consoleObj => {
-    console.log(consoleObj.text())
-  })
-
-  // 等待页面元素加载完成
-  await page.waitForSelector(selector)
-
-  // 解析页面元素
-  movie = await page.$$eval(selector, element => {
-    const result: Movie[] = []
-
-    element.forEach(ele => {
-      const posterEle = ele.querySelector('.post-box-image') as Element
-      let poster = posterEle.getAttribute('style') as string
-      const match = poster.match(/.*(https:\/\/[0-9a-zA-Z.\/]*)\);/) || []
-
-      if (match.length > 1) {
-        poster = match[1]
-      } else {
-        poster = ''
-      }
-
-      result.push({
-        posterId: ele.getAttribute('id') || '',
-        poster,
-        title: ele.querySelector('h2 > a')?.innerHTML || '',
-        innner: ele.getAttribute('data-href') || '',
-        rate: 0
-      })
+    // 绑定 console
+    await page.on('console', consoleObj => {
+      console.log(consoleObj.text())
     })
-    return result
-  })
 
-  // 获取下一页
-  const nextSel = await page.$('#main > div.pagination_wrap > nav > div > a.next.page-numbers')
+    // 等待页面元素加载完成
+    await page.waitForSelector(selector)
 
-  // 判断下一页是否存在
-  // 存在则递归爬取下一页页面
-  if (nextSel) {
-    await sleep({ type: 'random', delay: 10, min: 1 })
-    // 获取下一页 href
-    const href = await page.$eval('#main > div.pagination_wrap > nav > div > a.next.page-numbers', item => { return item.getAttribute('href') }) || ''
-    if (href) {
-      movie = [...movie, ...await parsePage(page, href)]
+    // 解析页面元素
+    movie = await page.$$eval(selector, element => {
+      const result: Movie[] = []
+
+      element.forEach(ele => {
+        const posterEle = ele.querySelector('.post-box-image') as Element
+        let poster = posterEle.getAttribute('style') as string
+        const match = poster.match(/.*(https:\/\/[0-9a-zA-Z.\/]*)\);/) || []
+
+        if (match.length > 1) {
+          poster = match[1]
+        } else {
+          poster = ''
+        }
+
+        result.push({
+          posterId: ele.getAttribute('id') || '',
+          poster,
+          title: ele.querySelector('h2 > a')?.innerHTML || '',
+          innner: ele.getAttribute('data-href') || '',
+          rate: 0
+        })
+      })
+      return result
+    })
+
+    // 获取下一页
+    const nextSel = await page.$('#main > div.pagination_wrap > nav > div > a.next.page-numbers')
+
+    // 判断下一页是否存在
+    // 存在则递归爬取下一页页面
+    if (nextSel) {
+      await sleep({ type: 'random', delay: 10, min: 1 })
+      // 获取下一页 href
+      const href = await page.$eval('#main > div.pagination_wrap > nav > div > a.next.page-numbers', item => { return item.getAttribute('href') }) || ''
+      if (href) {
+        movie = [...movie, ...await parsePage(page, href)]
+      }
+    } else {
+      console.log('---------------已经没有下一页啦～---------------')
     }
-  } else {
-    console.log('---------------已经没有下一页啦～---------------')
+  } catch (error) {
+    console.log('---------------error---------------', error)
   }
 
   return movie
 }
 
 async function parsePageRate(page: puppeteer.Page, url: string): Promise<number> {
+  console.log('---------------log rate url---------------', url)
   const selector = '.post-content > div.entry > div.doulist-item > div > div > div.rating > span.rating_nums'
-  await page.goto(url, {
-    waitUntil: 'networkidle0'
-  })
-  const rate = await page.$eval(selector, element => element.innerHTML)
-  console.log('---------------rate---------------', rate)
-  return Number(rate)
+  try {
+    await page.goto(url, {
+      waitUntil: 'networkidle0'
+    })
+    const rate = await page.$eval(selector, element => element.innerHTML)
+    console.log('---------------rate---------------', rate)
+    return Number(rate)
+  } catch (error) {
+    console.log('---------------error---------------', error)
+    return 0
+  }
 }
 
 export default router
